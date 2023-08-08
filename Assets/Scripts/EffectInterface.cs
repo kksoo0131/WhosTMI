a21051fd8da894899c81d060fa0a2158410e4da9
Hwan007 <ghksdlco@gmail.com>
2023-08-08 14:48:02 +0900

    edit FixedEffectInterface

--- Assets/Scripts/EffectInterface.cs
+++ Assets/Scripts/EffectInterface.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public interface MoveEffectInterface
 {
-    public abstract bool Run(GameObject _object, Vector3 _start, Vector3 _end);
+    public abstract bool Run(GameObject _object, Vector3? _start, Vector3? _end);
     public abstract void Cancel();
     public abstract bool IsEnd();
 }
 
 public interface FixedEffectInterface
 {
-    public abstract bool Run(GameObject _object);
+    public abstract bool Run(GameObject _object, Vector3? _position);
     public abstract void Cancel();
     public abstract bool IsEnd();
 }
