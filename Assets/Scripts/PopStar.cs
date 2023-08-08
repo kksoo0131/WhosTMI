f1981dc4705f9e9ac55fabdc661ea5f6d0157fc2
Hwan007 <ghksdlco@gmail.com>
2023-08-08 17:16:47 +0900

    해골나오기, 별 나오기 클래스 생성 및 나선형으로 나오기 클래스 기본 완성

---
+++ Assets/Scripts/PopStar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopStar : FixedEffectInterface
+{
+    public bool Run(GameObject _object, Vector3? _position)
+    {
+        return false;
+    }
+
+    public void Cancel()
+    {
+        
+    }
+
+    public bool IsEnd()
+    {
+        return false;
+    }
+}
