5a84503217b0d2ef6d306b02f176db061c25ae9b
Hwan007 <ghksdlco@gmail.com>
2023-08-09 17:14:39 +0900

    change arrow btn if there is max level or min level.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,6 +10,9 @@ public class LevelManager : MonoBehaviour
     public static LevelManager instance = null;
     public TMP_Text LevelText;
     public TMP_Text BestScoreText;
+    private int maxLevel=4;
+    public GameObject UP;
+    public GameObject DOWN;
     public int Level { get; private set; }
     private void Awake()
     {
@@ -32,20 +35,30 @@ public class LevelManager : MonoBehaviour
         }
         else
             BestScoreText.text = "0.00";
+        DOWN.SetActive(false);
     }
 
     public void LevelUp()
     {
-        if(PlayerPrefs.HasKey("stage" + (Level+1).ToString() + "Score"))
+        if (PlayerPrefs.HasKey("stage" + (Level + 1).ToString() + "Score"))
         {
             float bestScore = PlayerPrefs.GetFloat("stage" + (Level + 1).ToString() + "Score");
-            SetLevel(Level + 1);
+            SetLevel(+1);
             BestScoreText.text = bestScore.ToString("F2");
         }
-        else if (PlayerPrefs.HasKey("stage" + Level.ToString() + "Score"))
+        else if (Level < maxLevel && PlayerPrefs.HasKey("stage" + Level.ToString() + "Score"))
         {
-            SetLevel(Level + 1);
+            SetLevel(+1);
             BestScoreText.text = "0.00";
+            // 최대 스테이지로 변경됨.
+            // UP 버튼 비활성화
+            UP.SetActive(false);
+        }
+        else
+        {
+            // 현재 최대 스테이지
+            // UP 버튼 비활성화
+            UP.SetActive(false);
         }
     }
 
@@ -54,14 +67,25 @@ public class LevelManager : MonoBehaviour
         if (Level > 1 && PlayerPrefs.HasKey("stage" + (Level - 1).ToString() + "Score"))
         {
             float bestScore = PlayerPrefs.GetFloat("stage" + (Level - 1).ToString() + "Score");
-            SetLevel(Level - 1);
+            SetLevel(-1);
             BestScoreText.text = bestScore.ToString("F2");
+            if (Level == 1) { DOWN.SetActive(false); }
+        }
+        else
+        {
+            // Level <= 1 인 경우
+            // Down 버튼 비활성화
+            DOWN.SetActive(false);
         }
     }
 
     private void SetLevel(int _level)
     {
-        LevelText.text = _level.ToString();
-        Level = _level;
+        Level += _level;
+        LevelText.text = Level.ToString();
+        // level이 -1이면 UP버튼 활성화
+        // level이 +1이면 Down버튼 활성화
+        if (_level == 1) { DOWN.SetActive(true); }
+        else if(_level == -1) { UP.SetActive(true); }
     }
 }
