5c4ba5ff5bb24263a261d7267121069245165817
kksoo0131 <a67636521@gmail.com>
2023-08-09 11:31:36 +0900

    comflict 해결

--- Assets/Scripts/PopStar.cs
+++ Assets/Scripts/PopStar.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class PopStar : FixedEffectInterface
 {
+    private bool mIsRun;
+    private GameObject mStar;
+
+    public PopStar()
+    {
+        mStar = Resources.Load<GameObject>("Prefabs/star");
+        mIsRun = false;
+    }
     public bool Run(GameObject _object, Vector3? _position)
     {
+        if (mIsRun == false)
+        {
+
+        }
+        mIsRun = true;
         return false;
     }
 
